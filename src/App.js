6b5a39363f4a7ba11497aaa77ce5e28210d02449
ajitrchv <67496650+ajitrchv@users.noreply.github.com>
2022-03-18 23:03:30 +0530

    added local storage of tokens

--- src/App.js
+++ src/App.js
@@ -4,19 +4,28 @@ import Layout from './components/Layout/Layout';
 import UserProfile from './components/Profile/UserProfile';
 import AuthPage from './pages/AuthPage';
 import HomePage from './pages/HomePage';
+import AuthContext from './store/AuthContext';
+import { useContext } from 'react';
+import { Redirect } from 'react-router-dom';
 
 function App() {
+const authCtx = useContext(AuthContext);
+
   return (
     <Layout>
       <Switch>
         <Route path='/' exact>
           <HomePage />
         </Route>
-        <Route path='/auth'>
+        {!authCtx.isLoggedIn && <Route path='/auth'>
           <AuthPage />
-        </Route>
+        </Route>}
         <Route path='/profile'>
-          <UserProfile />
+        {authCtx.isLoggedIn && <UserProfile />}
+        {!authCtx.isLoggedIn && <Redirect to='/auth' />}
+        </Route>
+        <Route path='#'>
+          <Redirect to='/' />
         </Route>
       </Switch>
     </Layout>
--- src/store/AuthContext.js
+++ src/store/AuthContext.js
@@ -9,16 +9,19 @@ const AuthContext = React.createContext({
 
 });
  export const AuthContextProvider = (props) => {
-
-    const [token, setToken] = useState();
+    const initialToken = localStorage.getItem('token'); //following codes set an initial tokrn frm last set
+    const [token, setToken] = useState(initialToken);
     const userIsLoggedIn = !!token;
     const loginHandler = (token) => {
         
         setToken(token);
+        localStorage.setItem('token', token); //cookies like stuff, storing data locally
+        
     }
     const logOutHandler = async() => {
         
         setToken(null);
+        localStorage.removeItem('token'); //clears only 'token'
 
     }
     const contextValue = {
